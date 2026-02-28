6f80c5c1d3bddc86b5b5b02f74f0a52a9fa0f199
Douglas Prevelige <dpreveli@adobe.com>
2026-02-28 14:05:05 -0500

    new defaultoffer blcok

---
+++ blocks/defaultoffer/defaultoffer.js
@@ -0,0 +1,15 @@
+export default function decorate(block) {
+  const textCell = block.querySelector(':scope div:nth-child(1) > div');
+
+  const content = document.createElement('div');
+  content.classList.add('defaultoffer-content');
+
+  if (textCell) {
+    content.innerHTML = textCell.innerHTML;
+  } else {
+    content.innerHTML = "PLACEHOLDER";
+  }
+
+  block.innerHTML = '';
+  block.append(content);
+}
